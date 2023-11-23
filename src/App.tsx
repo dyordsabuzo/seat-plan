c7842ff4c98234a084a4bf60a3cc6c962d454dca
George Abuzo <gabuzo@securecodewarrior.com>
2023-11-23 13:46:39 +1100

    Move routed endpoints in App

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,42 @@
 // import { calculateLeftRightBalance, calculateFrontBackBalance } from "./utils/WeightCalculator";
 // import ImportPlayers from './refactor/ImportPlayers';
 // import Tabs from './components/Tabs';
-import {BoatContextProvider} from './context/BoatContext';
-import SetupPage from "./pages/SetupPage";
+// import {BoatContextProvider} from './context/BoatContext';
+// import SetupPage from "./pages/SetupPage";
+import {SetupProvider} from "./context/SetupContext";
+import {BrowserRouter, Outlet, Route, Routes} from "react-router-dom";
+import RaceAgeCategory from "./pages/wizard/boat-setup/RaceAgeCategory";
+import RaceType from "./pages/wizard/boat-setup/RaceType";
+import RaceDistance from "./pages/wizard/boat-setup/RaceDistance";
+import RaceBoatSize from "./pages/wizard/boat-setup/RaceBoatSize";
+import PaddlerListUpload from "./pages/wizard/boat-setup/PaddlerListUpload";
+import SetupBoard from "./pages/wizard/boat-setup/SetupBoard";
 
 // import Sample from "./components/drag-and-drop/Sample";
 
 function App() {
     return (
-        <div>
-            <BoatContextProvider>
-                {/*<ImportPlayers/>*/}
-                <SetupPage/>
-            </BoatContextProvider>
-        </div>
+        // <div>
+        //     <BoatContextProvider>
+        //         {/*<ImportPlayers/>*/}
+        //         <SetupPage/>
+        //     </BoatContextProvider>
+        // </div>
+        <SetupProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Outlet/>}>
+                        <Route index element={<RaceAgeCategory/>}/>
+                        <Route path="category" element={<RaceAgeCategory/>}/>
+                        <Route path="type" element={<RaceType/>}/>
+                        <Route path="distance" element={<RaceDistance/>}/>
+                        <Route path="boat" element={<RaceBoatSize/>}/>
+                        <Route path="paddlers" element={<PaddlerListUpload/>}/>
+                        <Route path="setupboard" element={<SetupBoard/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </SetupProvider>
     );
 }
 

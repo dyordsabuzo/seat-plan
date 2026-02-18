c404f06d01c64a2966360465dabbdbf1347668d6
Dyords Abuzo <dyordsabuzo@gmail.com>
2026-02-18 16:04:55 +1000

    Attempt to fix Import button

--- src/pages/SetupHome.tsx
+++ src/pages/SetupHome.tsx
@@ -136,6 +136,7 @@ export default function SetupHome() {
     logger.debug("Rendering SetupHome with regatta", regatta, "and allRegattas", allRegattas);
     return (
         <div className={`p-6 max-w-[900px] mx-auto`}>
+            <input ref={fileInputRef} type="file" accept=".json,application/json" style={{display: 'none'}} onChange={importRegattaFromFile} />
             <header className={`flex items-center justify-between mb-6`}>
                 <div>
                     <h1 className={`text-2xl font-semibold`}>Regatta configurations</h1>
